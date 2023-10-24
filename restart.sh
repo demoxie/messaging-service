a51c759efe452ffdd80633535d393fc41e53d58a
Shadrach Adamu <shadrachadamuul@gmail.com>
2023-10-24 08:27:02 +0100

    wel

--- restart.sh
+++ restart.sh
@@ -3,5 +3,5 @@
 if sudo lsof -ti:7007; then
   sudo pm2 restart messaging-service --watch && sudo pm2 restart messaging-service --watch
 else
-  sudo pm2 start process.json
+  sudo pm2 start process.yml
 fi
